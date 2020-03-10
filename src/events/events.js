519ec3e93762b7e97e669ecd7767b7d3be803356
Leandro Azevedo <leandrorazevedo@gmail.com>
2020-03-10 10:04:10 -0300

    notificação zank

--- src/events/events.js
+++ src/events/events.js
@@ -1,23 +1,60 @@
 const { createEventAdapter } = require('@slack/events-api');
+const { WebClient } = require('@slack/web-api');
 
 const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
+const web = new WebClient(process.env.SLACK_TOKEN);
+
+const USER_ADMIN = 'UHFEWMZGD';
+const USER_BOT = 'UUU8AA42J';
 
 slackEvents.on('url_verification', (event, body) => {
   console.log(event);
   console.log(body);
 });
 
-slackEvents.on('app_mention', event => {
+slackEvents.on('app_mention', async event => {
   console.log('mention :');
   console.log(event);
+
+  if (event.type !== 'app_mention') {
+    return;
+  }
+
+  await botInteractive(event);
 });
 
-slackEvents.on('message', (event, body) => {
-  console.log(event);
-  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+slackEvents.on('message', async (event, body) => {
+  console.log('message');
+  console.log('event :', event);
   console.log('body : ', body);
+
+  if (event.bot_profile) {
+    return;
+  }
+
+  await botInteractive(event);
 });
 
 slackEvents.on('error', console.error);
 
 module.exports = slackEvents;
+
+async function botInteractive(event) {
+  const textWithoutUser = event.text.replace(`<@${USER_BOT}>`, '');
+  const user = `<@${event.user}>`;
+  const channel = event.channel;
+  let msg = `Olá ${user}. Não entendi o que você deseja. Ainda estou aprendendo...`;
+
+  if (textWithoutUser.toLowerCase().includes('olá')) {
+    msg = `Olá ${user} ! \nComo posso ajudar ?`;
+  } else if (textWithoutUser.toLowerCase().includes('bom dia')) {
+    msg = `Bom dia ${user} ! \nComo posso ajudar ?`;
+  } else if (textWithoutUser.toLowerCase().includes('boa tarde')) {
+    msg = `Boa tarde ${user} ! \nComo posso ajudar ?`;
+  } else if (textWithoutUser.toLowerCase().includes('boa noite')) {
+    msg = `Boa noite ${user} ! \nComo posso ajudar ?`;
+  } else {
+    await web.chat.postMessage({ channel: USER_ADMIN, text: `${user} envou uma mensagem ao slank : \n ${event.text}` });
+  }
+  const result = await web.chat.postMessage({ channel, text: msg });
+}
--- src/webClient/index.js
+++ src/webClient/index.js
@@ -1,5 +1,6 @@
 const { WebClient } = require('@slack/web-api');
 const express = require('express');
+const axios = require('axios').default;
 const web = new WebClient(process.env.SLACK_TOKEN);
 
 const router = express.Router();
@@ -21,7 +22,7 @@ const emojis = [
 const rand = (min, max) => Math.floor(Math.random() * max) + min;
 
 router.post('/lanche', async (req, res) => {
-  const channel = req.body.channel || 'bots';
+  const channel = req.body.channel || process.env.GROUP_CHANNEL || 'bots';
 
   const hoje = new Date().getDate();
 
@@ -38,6 +39,49 @@ router.post('/lanche', async (req, res) => {
     text = `${emojis[emoji]}  Turma ${turma} - Lanche !  ${emojis[emoji]}`;
     await web.chat.postMessage({ channel, text });
   }, 1000 * 60 * process.env.LANCHE_INTERVAL);
+
+  res.status(200).json(result);
+});
+
+router.post('/notify', async (req, res) => {
+  const { solicitacaoId, sistema, cliente, atendimento, usuarioId } = req.body;
+
+  if (!usuarioId) {
+    return res.status(400).json({ ok: false, message: 'usuarioId não informado ou incorreto.' });
+  }
+
+  let response;
+  let user;
+
+  try {
+    response = await axios.get('http://us-east1-siacbancohoras.cloudfunctions.net/api/v1/users');
+    user = response.data.users.filter(user => user.zankId === usuarioId)[0];
+  } catch (error) {
+    return res.status(400).json({ ok: false, message: 'usuarioId não localizado!' });
+  }
+
+  let blocks = [
+    {
+      type: 'section',
+      text: {
+        text: `_Olá <@${user.slackId}>!_ *Uma solicitação do ZANK foi encaminhada pra você.*`,
+        type: 'mrkdwn'
+      },
+      fields: []
+    }
+  ];
+
+  blocks[0].fields.push({ type: 'mrkdwn', text: '*Solicitação*' });
+  blocks[0].fields.push({ type: 'mrkdwn', text: '*Sistema*' });
+  blocks[0].fields.push({ type: 'plain_text', text: `${solicitacaoId}` });
+  blocks[0].fields.push({ type: 'plain_text', text: sistema });
+
+  blocks[0].fields.push({ type: 'mrkdwn', text: '*Cliente*' });
+  blocks[0].fields.push({ type: 'mrkdwn', text: '*Siac Atendimento*' });
+  blocks[0].fields.push({ type: 'plain_text', text: cliente });
+  blocks[0].fields.push({ type: 'plain_text', text: `${atendimento}` });
+
+  const result = await web.chat.postMessage({ channel: user.slackId, blocks });
 
   res.status(200).json(result);
 });
